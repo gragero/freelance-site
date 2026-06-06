d930d55c2325523e24d84af1a6802f7cb5df226d
youssef mohsen <thewonderfuljo80@gmail.com>
2026-06-06 08:31:36 +0300

    fix smtp port

--- config.py
+++ config.py
@@ -10,8 +10,9 @@ class Config:
     DEBUG = False
 
     MAIL_SERVER = "smtp.gmail.com"
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
+    MAIL_PORT = 465
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL = True
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
     MAIL_DEFAULT_SENDER = ("Abo 7gag Portfolio", os.environ.get("MAIL_USERNAME"))
